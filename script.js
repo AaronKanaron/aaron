01b98e8113721a7c6ad977a492a71add1846d9ee
Aaron <aaronkanaron@gmail.com>
2021-12-02 19:33:51 +0100

    Javascript, Modals and Issues with hero

--- script.js
+++ script.js
@@ -1,9 +1,66 @@
 
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', () => {
 	//Call handlers and APIs
 	onThisDay();
 });
 
+/*=-------------=*/
+/*=             =*/
+/*=  VARIABLES  =*/
+/*=             =*/
+/*=-------------=*/
+
+let articles = [
+
+];
+
+
+/*=--------------=*/
+/*=              =*/
+/*=   FRONTEND   =*/
+/*=              =*/
+/*=--------------=*/
+
+//Toggle modal function
+function toggleModal(id, event) {
+	var modal = document.getElementById(id);
+	modal.classList.toggle("open");
+}
+
+/*Check if url is in articles array 
+If it is, remove the item from the array.
+If it is not, save the url as well as the title to the array.
+*/
+
+function saveArticle(url, title){
+	let article = {
+		title: title,
+		url: url
+	}
+	
+	if(articles.find(article => article.url === url)){
+		console.log("An existing article is already in array! Index of article: " + articles.findIndex(article => article.url === url));
+		articles.splice(articles.findIndex(article => article.url === url), 1);
+		console.log("Removed article from array!");
+	} else{
+		console.log("Article added to array");
+		articles.push(article);
+		localStorage.setItem("articles", JSON.stringify(articles));
+	}
+
+	console.log(articles);
+}
+
+
+
+// function saveArticle(title, url) {
+// 	let article = {
+// 		title: title,
+// 		url: url
+// 	};
+// 	articles.push(article);
+// 	localStorage.setItem("articles", JSON.stringify(articles));
+// }
 
 /*=---------------=*/
 /*=               =*/
@@ -24,6 +81,36 @@ function randomInt(min,max){
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function renderText(element, text) {
+	if (typeof (document.getElementById(element)) != undefined && document.getElementById(element) != null) {
+		Array.from(document.querySelectorAll("#" + element)).forEach((element) => {
+			if (Array.isArray(text)) {
+				element.innerHTML = ""
+				text.map((map_element, index) => {
+					index += 1; //Match with the length of an array
+					if (text.length > index) {
+
+						element.innerHTML += map_element.name + ", "
+						console.log("Last index")
+					} else {
+						element.innerHTML += map_element.name;
+						console.log("Not last index")
+
+					}
+					console.log("array element provided length:", text.length, "- current index of element:", index)
+
+					// text.length == index ? element.innerHTML += map_element.name + ", " : 
+					console.log(text.length)
+				})
+			} else {
+				element.innerHTML = text;
+			}
+		})
+	} else {
+		console.log("%cNo Element found for: " + element, "color:#FF0000")
+	}
 }
 
 
